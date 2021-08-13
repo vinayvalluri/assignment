e537984d7a513fee2fdc183e45f3c2e37404acdb
vinayvalluri <54655210+vinayvalluri@users.noreply.github.com>
2021-08-13 21:55:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,9 +8,25 @@ function App() {
   const [clientId, setClientId] = useState(
     "FCJFswdAg3vyLfd4BrNNFzYDWfHWPRGc-VrT5DfgUwk"
   );
+  const [result, setResult] = useState([]);
 
-  function handleChange(event) {}
-  function handleSubmit(event) {}
+  function handleChange(event) {
+    setPhoto(event.target.value);
+  }
+  function handleSubmit(event) {
+    console.log(photo);
+
+    const url =
+      "https://api.unsplash.com/search/photo?page=1&query=" +
+      photo +
+      "&client_id=" +
+      clientId;
+
+    axios.get(url).then((response) => {
+      console.log(response);
+      setResult(response.data.result);
+    });
+  }
   return (
     <div className="App">
       <h1>Unsplash Photo search in React</h1>
@@ -23,6 +39,10 @@ function App() {
       <button onClick={handleSubmit} type="submit">
         Search
       </button>
+
+      {result.map((photo) => (
+        <img src={photo.urls.small} />
+      ))}
     </div>
   );
 }
